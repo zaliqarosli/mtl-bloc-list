cc56b019b4bc801d0b720ccb7c904eda6bc513e8
Zaliqa Rosli <zaliqarosli@gmail.com>
2024-11-13 11:54:06 -0500

    directory page cleanup

--- js/directory.js
+++ js/directory.js
@@ -1,7 +1,7 @@
 fetch("json/directory.json")
   .then((response) => response.json())
   .then((data) => {
-    const list = document.getElementById("directory-list-items");
+    const list = document.querySelector(".directory-list-items");
     const html = data.items
       .map((item) => `<li><a href="${item.href}">${item.name}</a></li>`)
       .join("");
