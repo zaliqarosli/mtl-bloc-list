e3760bd92e234f0fe3054cc0ae1e265a01efe49a
Zaliqa Rosli <zaliqarosli@gmail.com>
2024-12-03 21:22:22 -0500

    update with latest figma design

--- js/directory.js
+++ js/directory.js
@@ -1,9 +1,17 @@
-fetch("json/directory.json")
-  .then((response) => response.json())
+const json = "json/directory.json";
+
+fetch(json)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    return response.json();
+  })
   .then((data) => {
     const list = document.querySelector(".directory-list-items");
     const html = data.items
       .map((item) => `<li><a href="${item.href}">${item.name}</a></li>`)
       .join("");
     list.innerHTML = html;
-  });
+  })
+  .catch((error) => console.error(error.message));
